ed4999c408803074a9f8be28ca2d9f51bc11e499
Suchid <suchidp@cybage.com>
2022-10-14 09:52:04 +0530

    testcases

--- Online_sport/src/test/java/com/cybage/service/ManagersubscritionserviceTest.java
+++ Online_sport/src/test/java/com/cybage/service/ManagersubscritionserviceTest.java
@@ -1,5 +1,38 @@
 package com.cybage.service;
 
-public class ManagersubscritionserviceTest {
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.cybage.daos.Manager_subscriptionDao;
+import com.cybage.models.Batches;
+import com.cybage.models.Manager_subscription;
+import com.cybage.models.Sport;
+
+@SpringBootTest
+public class ManagersubscritionserviceTest {
+	
+	
+	@Autowired
+	Manager_subscriptionDao manager_subscriptionDao;
+	
+	@Test
+    void getallmanager_subscription() {
+    	// Mockito.when(repository.findAll()).thenReturn(new ArrayList<>());
+    	Mockito.doReturn(new ArrayList<>()).when(manager_subscriptionDao).findAll();
+    	List<Manager_subscription> Manager_subscriptionList= new ArrayList<>();
+    	Manager_subscription manager_subscription= new Manager_subscription(1, "10 percent off",new Sport(), 1,"cricket",  new Batches(),1,"Eveing");
+    	Manager_subscription manager_subscription1= new Manager_subscription(2, "30 percent off",new Sport(), 1, "hockey",  new Batches(),2,"Morning");
+        
+    	Manager_subscriptionList.add(manager_subscription);
+    	Manager_subscriptionList.add(manager_subscription1);
+
+        Mockito.doReturn(Manager_subscriptionList).when(manager_subscriptionDao).findAll();
+
+    	assertEquals(2,manager_subscriptionDao.findAll().size());
+    }
 }
--- Online_sport/src/test/java/com/cybage/service/PlayersubscriptionserviceTest.java
+++ Online_sport/src/test/java/com/cybage/service/PlayersubscriptionserviceTest.java
@@ -1,5 +1,7 @@
 package com.cybage.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -9,6 +11,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.cybage.daos.PlayersDao;
 import com.cybage.daos.PlayersubscriptionDao;
+import com.cybage.models.Manager;
 import com.cybage.models.Playersubscription;
 
 @SpringBootTest
@@ -27,7 +30,50 @@ public class PlayersubscriptionserviceTest {
 	        assertEquals(21,playersubscription.findById(21).get().getPlayersubscription_id());
 	    }
 	 
-	 
-	 
-	 
+	
+	    @Test
+	    void testselectSubscription() {
+	    	Playersubscription playersub=new Playersubscription();
+	    	playersub.setPlayersubscription_id(11);
+
+	        Mockito.doReturn( playersub).when(playersubscription).save(playersub);
+
+	        assertEquals(11,playersubscription.save(playersub).getPlayersubscription_id());
+	    }
+	   
+	    
+	    @Test
+	    void testgetPlayersubscription() {
+	    	List<Playersubscription> playersubscriptionList=new ArrayList<>();
+	    	Playersubscription playersubscription=new Playersubscription();
+	    	Playersubscription playersubscription1=new Playersubscription();
+	    	Playersubscription playersubscription2=new Playersubscription();
+
+	    	playersubscriptionList.add(playersubscription);
+	    	playersubscriptionList.add(playersubscription1);
+	    	playersubscriptionList.add(playersubscription2);
+
+	        Mockito.doReturn(playersubscriptionList).when(playersubscription).findAll();
+
+	        assertEquals(3, playersubscription.findAll().size());
+	        
+	    }
+	    
+	    
+	    @Test
+	    void findPlayersubscriptionBymanagerId() {
+	    	List<Playersubscription> playersubscriptionList=new ArrayList<>();
+	        Manager manager=new Manager();
+	        Playersubscription playersubscription=new Playersubscription();
+	        playersubscription.setSubscription(manager);
+	        Playersubscription playersubscription1=new Playersubscription();
+	        playersubscription1.setSubscription(manager);
+
+	        playersubscriptionList.add(playersubscription);
+	        playersubscriptionList.add(playersubscription1);
+
+	        Mockito.doReturn(playersubscriptionList).when(playersubscription).getSubscription();
+
+	        assertEquals(2, playersubscription.getSubscription().size());
+	    }
 }
